5830fc5bb75823b17b1493b299ccedc30ea7b207
Alek94s <alek_straumsheim@hotmail.com>
2017-03-20 12:37:23 +0100

    almost done

--- src/wms/Innlogingsportal.java
+++ src/wms/Innlogingsportal.java
@@ -1,7 +1,78 @@
-package wms;
-
-/**
+package wms;/**
  * Created by aleksander on 3/20/17.
  */
-public class Innlogingsportal {
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+
+public class Innlogingsportal extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        primaryStage.setTitle("Login");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Button btn = new Button("Logg inn");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1 ,6);
+
+        Scene scene = new Scene(grid, 400, 275);
+
+        Text scenetitle = new Text("Login");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("Username:");
+        grid.add(userName, 0 , 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label password = new Label("Password:");
+        grid.add(password, 0 ,2);
+
+        PasswordField passwordBox = new PasswordField();
+        grid.add(passwordBox, 1, 2);
+
+
+
+
+
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setWidth(primaryStage.getWidth());
+        primaryStage.setHeight(primaryStage.getHeight());
+
+
+    }
 }
